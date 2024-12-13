35a7f3553e90c20b075451b061d4389af3a43d6e
CATT <weremito.business@gmail.com>
2024-12-13 22:32:38 +0900

    Update MisakiApp.swift

--- Misaki/MisakiApp.swift
+++ Misaki/MisakiApp.swift
@@ -9,17 +9,15 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
 }
-
 
 @main
 struct MisakiApp: App {
-    // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
@@ -28,3 +26,4 @@ struct MisakiApp: App {
         }
     }
 }
+
